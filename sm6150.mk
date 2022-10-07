b0d0f427d7254d6e184caefae6f7a50f449036c5
klozz <carlosj@klozz.dev>
2022-10-07 09:55:11 +0200

    sm6150-common: build libldacBT_bco missing
    Signed-off-by: klozz <carlosj@klozz.dev>

--- sm6150.mk
+++ sm6150.mk
@@ -106,6 +106,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     libbthost_if \
+    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
