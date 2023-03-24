c48e234a807e0d1692d60497a426c66bcd89ac64
klozz <carlosj@klozz.dev>
2023-03-24 01:35:41 +0000

    sm6150-common:Drop android.hardware.bluetooth.audio-impl
     * This fix AAC aptx adaptive and ldac offload issue (this cause no audio on bt)
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- sm6150.mk
+++ sm6150.mk
@@ -102,7 +102,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.1-impl:32 \
-    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     libbthost_if \
     libldacBT_bco \
